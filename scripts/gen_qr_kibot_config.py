e3e59faec2d99f2175b755429553add3a8f8c84b
Jasper Chan <jasperchan515@gmail.com>
2022-08-31 21:39:52 -0700

    ci: generate kibot config to generate qr code

---
+++ scripts/gen_qr_kibot_config.py
@@ -0,0 +1,71 @@
+import os
+from datetime import datetime
+import yaml
+
+URL = "https://github.com/Gigahawk/clickwheel_breakout_4th_gen/tree"
+
+print("Generating QR text")
+
+date = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+
+hash_full = os.environ["GITHUB_SHA"]
+hash_short = hash_full[:7]
+
+ref_type = os.environ.get("GITHUB_REF_TYPE")
+if ref_type == "tag":
+    tag = os.environ.get("GITHUB_REF_NAME")
+else:
+    tag = None
+
+msg = f"""Date: {date}
+Link: {URL}/{hash_short}"""
+
+if tag != None:
+    msg += f"\nTag: {tag}"
+
+print("#############")
+print(msg)
+print("#############")
+
+print("Writing KiBot config to generate QR code")
+config = {
+    "kibot": {
+        "version": 1
+    },
+    "outputs": [
+        {
+            "name": "QR Data Output",
+            "type": "qr_lib",
+            "dir": "QR_libs",
+            "options": {
+                "use_sch_dir": False,
+                "qrs": [
+                    {
+                        "name": "QR_data",
+                        "correction_level": "low",
+                        "size_pcb": 10,
+                        "size_sch": 20,
+                        "size_units": "millimeters",
+                        "text": msg,
+                    }
+                ]
+            }
+        }
+    ]
+}
+
+output = yaml.dump(config)
+print("#############")
+print(output)
+print("#############")
+
+with open("kibot_qr.yaml", "w") as f:
+    f.write(output)
+
+
+
+
+
+
+
+
